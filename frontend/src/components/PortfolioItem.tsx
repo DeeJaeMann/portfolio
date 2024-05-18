6a037065595aa82b48ce630b1eb21203b83f581a
Dee J. Mann <dee.j.mann@hotmail.com>
2024-05-18 14:21:11 -0400

    Updated formatting Changes to be committed: 	modified:   ../../src/components/PortfolioItem.tsx

--- frontend/src/components/PortfolioItem.tsx
+++ frontend/src/components/PortfolioItem.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 
 interface PortfolioItemProps {
-    title: string;
-    imgUrl: string;
-    stack: string[];
-    link: string;
+  title: string;
+  imgUrl: string;
+  stack: string[];
+  link: string;
 }
 
-const PortfolioItem: React.FC<PortfolioItemProps> = ({ title = 'Missing', imgUrl = '', stack = ['None'], link = '' }) => {
+const PortfolioItem: React.FC<PortfolioItemProps> = ({
+  title = "Missing",
+  imgUrl = "",
+  stack = ["None"],
+  link = "",
+}) => {
   return (
     <div className="border-2 border-stone-900 rounded-md overflow-hidden">
       <img

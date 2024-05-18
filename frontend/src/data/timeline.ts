6736accd4de0780c6e21df0d12b457ad61674f64
Dee J. Mann <dee.j.mann@hotmail.com>
2024-05-18 15:19:14 -0400

    Added TimelineItem components Added timeline.ts for data Changes to be committed: 	new file:   ../components/TimelineItem.tsx 	new file:   timeline.ts

---
+++ frontend/src/data/timeline.ts
@@ -0,0 +1,26 @@
+export default [
+    {
+        year: 'Today',
+        title: 'Software Engineer',
+        duration: 'Something',
+        details: 'Make sure to refactor all of this to be software related',
+    },
+    {
+        year: '2024',
+        title: 'Quality Inspector',
+        duration: '8 years',
+        details: 'Quality job details',
+    },
+    {
+        year: '2015',
+        title: 'Army Title',
+        duration: '13 years',
+        details: 'Army job details',
+    },
+    {
+        year: '2001',
+        title: 'CGI job',
+        duration: '2 years',
+        details: 'CGI details',
+    },
+]

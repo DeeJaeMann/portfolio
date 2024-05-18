98b9bb1858cdeeaa1f935b8633e08b864676922a
Dee J. Mann <dee.j.mann@hotmail.com>
2024-05-18 15:55:33 -0400

    Added contact form Changes to be committed: 	modified:   ../components/Contact.tsx

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-    return(
-        <h2>Contact</h2>
-    );
-}
+  return (
+    <div className="flex flex-col mb-10 mx-auto">
+      <div className="flex justify-center items-center">
+        <form
+          action="https://getform.io/f/lbjkvjwa"
+          method="POST"
+          className="flex flex-col w-full md:w-7/12"
+        >
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none"
+          />
+          <textarea
+            name="message"
+            placeholder="Message"
+            rows={10}
+            className="mb-4 p-2 bg-transparent border-2 rounded-md focus:outline-none"
+          />
+          <button
+            type="button"
+            className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-cyan-600 to-blue-900 drop-shadow-md hover:stroke-white"
+          >
+            Work With Me
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;

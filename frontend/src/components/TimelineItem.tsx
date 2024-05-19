43696dcda63c58ccf33369687754938771ef5df2
Dee J. Mann <dee.j.mann@hotmail.com>
2024-05-19 12:03:21 -0400

    Changed spans to badges for timeline years Changes to be committed: 	modified:   src/components/TimelineItem.tsx

--- frontend/src/components/TimelineItem.tsx
+++ frontend/src/components/TimelineItem.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Badge } from "@/components/ui/badge";
 
 interface TimelineItemProps {
   year: string;
@@ -18,15 +19,17 @@ const TimelineItem: React.FC<TimelineItemProps> = ({
       <li className="mb-10 ml-4">
         <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border border-white dark:bg-stone-700 dark:border-stone-900" />
         <p className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
-          <span className="inline-block px-2 py-1 font-semibold text-white bg-stone-900 rounded-md dark:bg-white dark:text-stone-900">
-            {year}
-          </span>
-          <h3 className="text-lg font-semibold text-stone-900 dark:text-white">{title}</h3>
+          <Badge>{year}</Badge>
+          <h3 className="text-lg font-semibold text-stone-900 dark:text-white">
+            {title}
+          </h3>
           <div className="my-1 text-sm font-normal leading-none text-stone-400 dark:text-stone-500">
             {duration}
           </div>
         </p>
-        <p className="my-2 text-base font-normal text-stone-500 dark:text-stone-400">{details}</p>
+        <p className="my-2 text-base font-normal text-stone-500 dark:text-stone-400">
+          {details}
+        </p>
       </li>
     </ol>
   );

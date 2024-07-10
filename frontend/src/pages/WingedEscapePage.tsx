f4fafe6ee8937a0eb15258d5efb0ee31fbb4da35
Dee J. Mann <dee.j.mann@hotmail.com>
2024-07-10 15:16:25 -0400

    Added WingedEscapePage Connected page to router Changes to be committed: 	new file:   src/pages/WingedEscapePage.tsx 	modified:   src/router.tsx

---
+++ frontend/src/pages/WingedEscapePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
+
+const WingedEscapePage = () => {
+    return (
+        <>
+            <p>Winged Escape Page</p>
+        </>
+    )
+};
+
+export default WingedEscapePage

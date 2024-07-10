ecbf1d8e85d29dd7c9307b35024dc371973800bd
Dee J. Mann <dee.j.mann@hotmail.com>
2024-07-10 15:33:26 -0400

    Connected Unity to React Changes to be committed: 	modified:   src/pages/WingedEscapePage.tsx

--- frontend/src/pages/WingedEscapePage.tsx
+++ frontend/src/pages/WingedEscapePage.tsx
@@ -2,9 +2,19 @@ import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 const WingedEscapePage = () => {
+
+    const { unityProvider } = useUnityContext({
+        loaderUrl: "/build/WebGL-v1.1.loader.js",
+        dataUrl: "/build/WebGL-v1.1.data.br",
+        frameworkUrl: "/build/WebGL-v1.1.framework.js.br",
+        codeUrl: "/build/WebGL-v1.1.wasm.br",
+    });
+
     return (
         <>
-            <p>Winged Escape Page</p>
+            <center>
+            <Unity unityProvider={unityProvider} className="max-w-5l w-11/12 mx-auto" />
+            </center>
         </>
     )
 };
